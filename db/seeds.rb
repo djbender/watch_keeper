2b6a138d2e24d84d821d50f293deebe74d9da6b3
Derek Bender <170351+djbender@users.noreply.github.com>
2025-07-07 16:39:36 -0700

    Add @hotwired/turbo-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%W(Seiko Spinnaker Citizen).each do |name|
+  Watch.create!(name: name)
+end
